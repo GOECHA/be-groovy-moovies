8e2ddbbc23f8d4f1288dd87dece9814797a995f5
Chantal Goethals <chantalgoethals@gmail.com>
2023-08-23 14:51:49 -0600

    vercel

--- index.js
+++ index.js
@@ -1,93 +1,53 @@
-
-
-// Entry Point of the API Server
-
 const express = require('express');
+const { Pool } = require('pg');
 
-/* Creates an Express application.
-The express() function is a top-level
-function exported by the express module.
-*/
 const app = express();
-const Pool = require('pg').Pool;
-
-
 const pool = new Pool({
     connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-  })
+});
 
+app.use(express.json());
 
-/* To handle the HTTP Methods Body Parser
-is used, Generally used to extract the
-entire body portion of an incoming
-request stream and exposes it on req.body
-*/
-const bodyParser = require('body-parser');
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/getMovies', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM addMovies');
+        res.json(result.rows);
+    } catch (error) {
+        console.error('Error fetching movies:', error);
+        res.status(500).send('Error fetching movies');
+    }
+});
 
-
-pool.connect((err, client, release) => {
-	if (err) {
-		return console.error(
-			'Error acquiring client', err.stack)
-	}
-	client.query('SELECT NOW()', (err, result) => {
-		release()
-		if (err) {
-			return console.error(
-				'Error executing query', err.stack)
-		}
-		console.log("Connected to Database !")
-	})
-})
-
-
-// Require the Routes API
-// Create a Server and run it on the port 3000
-const server = app.listen(3000, function () {
-	let host = server.address().address
-	let port = server.address().port
-	// Starting the Server at the port 3000
-})
-
-app.post('/addMovies', (req, res, next) => {
-    const moviesToInsert = req.body.movies; // Assuming the request body contains the "movies" array
+app.post('/addMovies', async (req, res) => {
+    const moviesToInsert = req.body.movies;
 
     const insertQuery = 'INSERT INTO addMovies (id, poster_path, backdrop_path, title, average_rating, release_date) VALUES ($1, $2, $3, $4, $5, $6)';
 
-    const insertPromises = moviesToInsert.map(movie => {
-        const values = [
-            movie.id,
-            movie.poster_path,
-            movie.backdrop_path,
-            movie.title,
-            movie.average_rating,
-            movie.release_date
-        ];
-        return pool.query(insertQuery, values);
-    });
+    try {
+        await Promise.all(moviesToInsert.map(async (movie) => {
+            const values = [
+                movie.id,
+                movie.poster_path,
+                movie.backdrop_path,
+                movie.title,
+                movie.average_rating,
+                movie.release_date,
+            ];
+            await pool.query(insertQuery, values);
+        }));
 
-    Promise.all(insertPromises)
-        .then(results => {
-            console.log('Movies inserted successfully');
-            res.send('Movies inserted successfully');
-        })
-        .catch(error => {
-            console.error('Error inserting movies:', error);
-            res.status(500).send('Error inserting movies');
-        });
+        console.log('Movies inserted successfully');
+        res.send('Movies inserted successfully');
+    } catch (error) {
+        console.error('Error inserting movies:', error);
+        res.status(500).send('Error inserting movies');
+    }
 });
 
-app.get('/getMovies', (req, res, next) => {
-    pool.query('SELECT * FROM addMovies')
-        .then(result => {
-            res.json(result.rows);
-        })
-        .catch(error => {
-            console.error('Error fetching movies:', error);
-            res.status(500).send('Error fetching movies');
-        });
+console.log('Starting server...');
+const server = app.listen(3000, () => {
+    console.log('Server started on port 3000');
 });
 
-module.exports = pool
+
+module.exports = app; // Export the Express app
